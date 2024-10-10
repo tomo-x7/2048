6759936371fe955c7952140261840c8e04bebc93
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-10-11 03:52:02 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,15 @@ function App() {
 		bottom: [],
 	});
 	const [data, setdata] = useState<string[][]>();
+	const mainref = useRef<HTMLDivElement>(null);
+	const [holdstate,sethold]=useState<"l"|"r"|"t"|"b"|"n"|undefined>()
 	useEffect(() => {
 		const arr1: string[][] = [];
 		for (let i = 0; i < size; i++) {
 			const arr2: string[] = [];
 			for (let j = 0; j < size; j++) {
 				const key = crypto.randomUUID();
-				rawdata.current[key] = new cell(10 * i + j);
+				rawdata.current[key] = new cell(2 ** Math.ceil(Math.random() * 10));
 				arr2.push(key);
 			}
 			arr1.push(arr2);
@@ -48,18 +50,43 @@ function App() {
 	const getrandom = () => crypto.getRandomValues(new Uint16Array(1))[0].toString();
 	return (
 		<>
-			<div style={{ display: "flex", flexDirection: "column",aspectRatio:1,maxWidth:"400px",border:"solid black 3px" }}>
+			<div
+				ref={mainref}
+				style={{
+					display: "flex",
+					flexDirection: "column",
+					aspectRatio: 1,
+					maxWidth: "400px",
+					width: "100%",
+					border: "solid black 3px",
+				}}
+			>
 				{data?.map((arr) => (
-					<div key={getrandom()} style={{ display: "flex",flex:1 }}>
+					<div key={getrandom()} style={{ display: "flex", flex: 1 }}>
 						{arr.map((key) => (
-							<div key={getrandom()} style={{display:"flex",flex:1,alignItems:"center",justifyContent:"center",border:"solid black 3px"}}>
+							<div
+								key={getrandom()}
+								style={{
+									display: "flex",
+									flex: 1,
+									alignItems: "center",
+									justifyContent: "center",
+									border: "solid black 3px",
+								}}
+							>
 								{rawdata.current[key].num}
 							</div>
 						))}
 					</div>
 				))}
 			</div>
-			<Buttons action={() => {}} />
+			<Buttons
+				action={() => {
+					setdata(data?.slice());
+				}}
+				mainref={mainref} sethold={sethold}
+			/>
+			<div>{holdstate}</div>
 		</>
 	);
 }
--- src/Buttons.tsx
+++ src/Buttons.tsx
@@ -1,8 +1,35 @@
-export function Buttons({ action }: { action: (type: "right" | "left" | "top" | "bottom") => unknown }) {
+import { useEffect, useRef } from "react";
+
+export function Buttons({
+	action:actiontest,
+	mainref,sethold
+}: {
+	action: (type: "right" | "left" | "top" | "bottom") => unknown;
+	mainref: React.RefObject<HTMLDivElement>;
+	sethold: React.Dispatch<React.SetStateAction<"l" | "r" | "t" | "b" | "n" | undefined>>;
+}) {
+	const action=(a:"right" | "left" | "top" | "bottom")=>{
+		sethold(a[0] as ("r"|"l"|"t"|"b"))
+		actiontest(a)
+	}
 	const top = () => action("top");
 	const right = () => action("right");
 	const left = () => action("left");
 	const bottom = () => action("bottom");
+	type pos = { x: number; y: number; identifier: number };
+	const touchpos = useRef<{ start?: pos; end?: pos }>({});
+	useEffect(() => {
+		mainref.current?.addEventListener("touchstart", (ev) => {
+			const t = ev.changedTouches[0];
+			touchpos.current.start = { identifier: t.identifier, x: t.clientX, y: t.clientY };
+		});
+		mainref.current?.addEventListener("touchmove", (ev) => {
+			const t = Array.from(ev.changedTouches).find((v) => v.identifier === touchpos.current.start?.identifier);
+			touchpos.current.end = t ? { identifier: t.identifier, x: t.clientX, y: t.clientY } : undefined;
+			
+		});
+	}, [mainref]);
+
 	return (
 		<>
 			<button type="button" onClick={top}>
